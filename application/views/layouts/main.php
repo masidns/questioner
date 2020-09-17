cc49ee6fe791b128bb7b56d3366bbf75953341c5
kristt26 <kristt26@gmail.com>
2020-09-17 19:42:16 +0900

    new

--- application/views/layouts/main.php
+++ application/views/layouts/main.php
@@ -3,16 +3,16 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>Questuiner | <?= $title['title']?></title>
+  <title>Questuiner | <?=$title['title']?></title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="<?= base_url()?>assets/plugins/fontawesome-free/css/all.min.css">
+  <link rel="stylesheet" href="<?=base_url()?>assets/plugins/fontawesome-free/css/all.min.css">
   <!-- Ionicons -->
   <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
   <!-- overlayScrollbars -->
-  <link rel="stylesheet" href="<?= base_url()?>assets/dist/css/adminlte.min.css">
+  <link rel="stylesheet" href="<?=base_url()?>assets/dist/css/adminlte.min.css">
   <!-- Google Font: Source Sans Pro -->
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
 </head>
@@ -27,7 +27,7 @@
         <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
       </li>
       <li class="nav-item d-none d-sm-inline-block">
-        <a href="<?= base_url()?>assets/index3.html" class="nav-link">Home</a>
+        <a href="<?=base_url()?>assets/index3.html" class="nav-link">Home</a>
       </li>
       <li class="nav-item d-none d-sm-inline-block">
         <a href="#" class="nav-link">Contact</a>
@@ -58,7 +58,7 @@
           <a href="#" class="dropdown-item">
             <!-- Message Start -->
             <div class="media">
-              <img src="<?= base_url()?>assets/dist/img/user1-128x128.jpg" alt="User Avatar" class="img-size-50 mr-3 img-circle">
+              <img src="<?=base_url()?>assets/dist/img/user1-128x128.jpg" alt="User Avatar" class="img-size-50 mr-3 img-circle">
               <div class="media-body">
                 <h3 class="dropdown-item-title">
                   Brad Diesel
@@ -74,7 +74,7 @@
           <a href="#" class="dropdown-item">
             <!-- Message Start -->
             <div class="media">
-              <img src="<?= base_url()?>assets/dist/img/user8-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
+              <img src="<?=base_url()?>assets/dist/img/user8-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
               <div class="media-body">
                 <h3 class="dropdown-item-title">
                   John Pierce
@@ -90,7 +90,7 @@
           <a href="#" class="dropdown-item">
             <!-- Message Start -->
             <div class="media">
-              <img src="<?= base_url()?>assets/dist/img/user3-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
+              <img src="<?=base_url()?>assets/dist/img/user3-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
               <div class="media-body">
                 <h3 class="dropdown-item-title">
                   Nora Silvester
@@ -145,8 +145,8 @@
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="<?= base_url()?>assets/index3.html" class="brand-link">
-      <img src="<?= base_url()?>assets/dist/img/AdminLTELogo.png"
+    <a href="<?=base_url()?>assets/index3.html" class="brand-link">
+      <img src="<?=base_url()?>assets/dist/img/AdminLTELogo.png"
            alt="AdminLTE Logo"
            class="brand-image img-circle elevation-3"
            style="opacity: .8">
@@ -158,7 +158,7 @@
       <!-- Sidebar user (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="<?= base_url()?>assets/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+          <img src="<?=base_url()?>assets/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
           <a href="#" class="d-block">Alexander Pierce</a>
@@ -188,21 +188,21 @@
             </a>
             <ul class="nav nav-treeview">
               <li class="nav-item">
-                <a href="<?= base_url("aspek_penilaian")?>" class="nav-link">
+                <a href="<?=base_url("aspek_penilaian")?>" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
                   <p>Aspek Penilaian</p>
                 </a>
               </li>
               <li class="nav-item">
-                <a href="<?= base_url("range_nilai")?>" class="nav-link">
+                <a href="<?=base_url("range_nilai")?>" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
                   <p>Range Nilai</p>
                 </a>
               </li>
               <li class="nav-item">
-                <a href="../layout/boxed.html" class="nav-link">
+                <a href="<?=base_url("layanan")?>" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
-                  <p>Boxed</p>
+                  <p>Layanan</p>
                 </a>
               </li>
               <li class="nav-item">
@@ -274,12 +274,12 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1><?= $title['title']?></h1>
+            <h1><?=$title['title']?></h1>
           </div>
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
               <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active"><?= $title['title']?></li>
+              <li class="breadcrumb-item active"><?=$title['title']?></li>
             </ol>
           </div>
         </div>
@@ -288,10 +288,12 @@
 
     <!-- Main content -->
     <section class="content">
-    <?php                    
-                    if(isset($_view) && $_view)
-                        $this->load->view($_view);
-                    ?>  
+    <?php
+if (isset($_view) && $_view) {
+    $this->load->view($_view);
+}
+
+?>
     </section>
     <!-- /.content -->
   </div>
@@ -314,18 +316,18 @@
 <!-- ./wrapper -->
 
 <!-- jQuery -->
-<script src="<?= base_url()?>assets/plugins/jquery/jquery.min.js"></script>
-<script src="<?= base_url()?>assets/lib/angular/angular.min.js"></script>
-<script src="<?= base_url('assets/js/apps.js')?>"></script>
-<script src="<?= base_url('assets/js/controllers/controllers.js')?>"></script>
-<script src="<?= base_url('assets/js/services/services.js')?>"></script>
-<script src="<?= base_url('assets/js/services/helper.service.js')?>"></script>
+<script src="<?=base_url()?>assets/plugins/jquery/jquery.min.js"></script>
+<script src="<?=base_url()?>assets/lib/angular/angular.min.js"></script>
+<script src="<?=base_url('assets/js/apps.js')?>"></script>
+<script src="<?=base_url('assets/js/controllers/controllers.js')?>"></script>
+<script src="<?=base_url('assets/js/services/services.js')?>"></script>
+<script src="<?=base_url('assets/js/services/helper.service.js')?>"></script>
 <!-- Bootstrap 4 -->
-<script src="<?= base_url()?>assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="<?=base_url()?>assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 <!-- AdminLTE App -->
-<script src="<?= base_url()?>assets/dist/js/adminlte.min.js"></script>
+<script src="<?=base_url()?>assets/dist/js/adminlte.min.js"></script>
 <!-- AdminLTE for demo purposes -->
-<script src="<?= base_url()?>assets/dist/js/demo.js"></script>
+<script src="<?=base_url()?>assets/dist/js/demo.js"></script>
 <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 </body>
 </html>
